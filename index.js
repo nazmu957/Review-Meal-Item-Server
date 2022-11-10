74448e91fba0dff268a867a1fdf9c7ae8f1548d3
nazmu957 <nazmus15-1999@diu.edu.bd>
2022-11-10 21:33:47 +0600

    update service api

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ async function run(){
         app.get('/services', async(req, res) =>{
             const query = {}
             const cursor = serviceCollection.find(query);
-            const services = await cursor.limit(3).toArray();
+            const services = await cursor.toArray();
             res.send(services);
         });
         app.get('/services/:id', async(req, res) =>{
